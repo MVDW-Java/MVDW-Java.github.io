349963956e51c4eb2a79a1e9ef575861b60a5608
MVDW <maxvdwolf@gmail.com>
2026-09-16 21:07:23 +0200

    zet site in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://backontrack.maxvanderwolf.nl',
     vite: {
         plugins: [tailwindcss()],
     },
